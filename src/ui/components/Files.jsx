a6ea956613c7e9b211cb42056db425b3d7c99a00
Krishna Rathnam <kr130205@gmail.com>
2025-04-23 23:40:14 +0530

    thirst commit

--- src/ui/components/Files.jsx
+++ src/ui/components/Files.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Files() {
+export default function Files({changeIndex, indexs}) {
     const navItems = [
         "Home",
         "Quantum computing",
@@ -12,7 +12,12 @@ export default function Files() {
         "Zap ⚡",
         "Creative writing",
         "All projects",
+        "Quantum computing",
+        "Quantum computing",
+        "Quantum computing",
     ];
+
+
     return (
         <div className="w-60 bg-off-white px-4 border-r overflow-y-auto ">
             <ul className="space-y-2">
@@ -20,10 +25,11 @@ export default function Files() {
                     <button
                         key={index}
                         className={`text-sm block w-full px-3 py-2 rounded-l cursor-pointer 
-            ${index === 1
+                                ${index === indexs
                                 ? "bg-white text-off-black font-medium shadow-sm"
                                 : "text-dim-gray hover:text-off-black hover:bg-gray-100"
                             }`}
+                        onClick={() => changeIndex(index)}
                     >
                         {item}
                     </button>
--- src/ui/components/SideBar.jsx
+++ src/ui/components/SideBar.jsx
@@ -1,11 +1,19 @@
 import Header from "./Header";
 import Files from "./Files";
-const SideBar = ({ children }) => {
+import { useState } from "react";
+const SideBar = () => {
+  const [indexs, setIndex] = useState(1)
+
+  function changeIndex(indi){
+      setIndex(indi);
+        console.log("Index changed")
+        console.log(indexs)
+  }
 
   return (
     <aside className="h-screen w-55 bg-off-white m-0">
       <Header />
-      <Files />
+      <Files changeIndex={changeIndex} setIndex={setIndex} indexs={indexs}/>
     </aside>
   );
 };
